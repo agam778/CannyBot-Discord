b3fe3e24b8604db43925ea2919bb5a93143ba8f7
Agampreet Singh <agam778singh@outlook.com>
2022-02-06 11:45:29 +0530

    Add embed command

--- commands/Fun/embed.js
+++ commands/Fun/embed.js
@@ -1,20 +1,41 @@
+const { MessageEmbed } = require("discord.js");
+const ee = require("../../botconfig/embed.json");
+
 module.exports = {
   name: "embed",
   category: "Fun",
   aliases: [],
   cooldown: "",
-  usage: "",
+  usage: "embed title | description | imageurl",
   description: "Resends your Text in an embed",
   memberpermissions: [],
   requiredroles: [],
   alloweduserids: [],
-  minargs: 0,
+  minargs: 1,
   maxargs: 0,
-  minplusargs: 1,
+  minplusargs: 0,
   maxplusargs: 0,
   argsmissing_message: "",
   argstoomany_message: "",
   run: async (client, message, args, plusArgs, cmdUser, text, prefix) => {
-    message.reply("Please use slash command for this command - `/embed`");
+    let embed = new MessageEmbed();
+    let embed_args = args.join(" ").split("|");
+    embed.setTitle(embed_args[0]);
+    embed_args[1] ? embed.setDescription(embed_args[1]) : null;
+    embed_args[2] ? embed.setImage(embed_args[2]) : null;
+    embed.setColor(ee.color);
+    embed.setFooter({
+      text: `${message.guild.name}`,
+      iconURL: `${message.guild.iconURL()}`,
+    });
+    embed.setAuthor({
+      name: message.author.tag,
+      iconURL: message.author.displayAvatarURL(),
+    });
+    message.channel.send({ embeds: [embed] }).catch((err) => {
+      message.reply(
+        `Oops! An Error Occured!\n\`\`\`js\n${err}\n\`\`\`\nPlease follow this format to create embed: \`${prefix}embed title | description | imageurl\``
+      );
+    });
   },
 };
---
+++ slashCommands/embed.js
@@ -0,0 +1,76 @@
+const { MessageEmbed } = require("discord.js");
+const ee = require("../botconfig/embed.json");
+module.exports = {
+  name: "embed",
+  description: "Generate an Embed", //the command description for Slash Command Overview
+  cooldown: [],
+  memberpermissions: ["KICK_MEMBERS"],
+  requiredroles: [],
+  alloweduserids: [],
+  options: [
+    {
+      String: {
+        name: "title",
+        description: "The title of the embed",
+        required: true,
+      },
+    },
+    {
+      String: {
+        name: "description",
+        description: "The description of the embed",
+        required: true,
+      },
+    },
+    {
+      String: {
+        name: "imageurl",
+        description: "The image url of the embed",
+        required: false,
+      },
+    },
+  ],
+  run: async (client, interaction) => {
+    const {
+      member,
+      channelId,
+      guildId,
+      applicationId,
+      commandName,
+      deferred,
+      replied,
+      ephemeral,
+      options,
+      id,
+      createdTimestamp,
+    } = interaction;
+    const embed = new MessageEmbed();
+    const title = options.getString("title");
+    const description = options.getString("description");
+    const imageurl = options.getString("imageurl");
+    embed.setAuthor({
+      name: interaction.user.tag,
+      iconURL: interaction.user.displayAvatarURL(),
+    });
+    embed.setTitle(title);
+    embed.setDescription(description);
+
+    if (imageurl && !imageurl.startsWith("http")) {
+      return interaction.reply({
+        content: "Image URL must start with http or https",
+        ephemeral: true,
+      });
+    }
+    imageurl ? embed.setImage(imageurl) : null;
+    embed.setColor(ee.color);
+    embed.setFooter({
+      text: `${interaction.guild.name}`,
+      iconURL: `${interaction.guild.iconURL()}`,
+    });
+    interaction.channel.send({ embeds: [embed] });
+    interaction.reply({
+      content: "Embed Sent! ✅",
+      ephemeral: true,
+    });
+  },
+};
